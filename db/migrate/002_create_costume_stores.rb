ed917050855f18377500c27baa2ad66ea38e968c
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-23 17:16:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,4 +2,14 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table costume_stores
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business 
+      t.integer :opening_time
+      t.integer :closing_time
+    end
+  end
+end
